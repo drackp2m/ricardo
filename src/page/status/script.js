59bfb29a7825b7fd697e98ba1a80aea8a12988af
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2025-05-19 00:32:03 +0200

    add header on status page

--- src/page/status/script.js
+++ src/page/status/script.js
@@ -2,9 +2,15 @@ import { main } from '../../script.js';
 import { sessionManager } from '../../script/session-manager.js';
 
 main.onReady(async () => {
-  const userStatus = (await sessionManager.getUserData()).status;
+  const nameElement = document.querySelector('#name');
 
-  switch(userStatus) {
+  const userData = await sessionManager.getUserData();
+
+  const { status, name, surname } = userData;
+
+  nameElement.textContent = `${name} ${surname}`;
+
+  switch (status) {
     case 'PENDING':
       const pendingParagraph = document.querySelector('#pending');
       pendingParagraph.classList.remove('hidden');
